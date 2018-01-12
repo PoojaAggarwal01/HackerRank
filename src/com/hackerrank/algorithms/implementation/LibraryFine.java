648dabbf03bd4bb7f6ab26713eefc96a2d8d3368
Pooja Aggarwal <poojaaggarwal292@gmail.com>
2018-01-12 13:10:12 +0530

    library fine

---
+++ src/com/hackerrank/algorithms/implementation/LibraryFine.java
@@ -0,0 +1,43 @@
+package com.hackerrank.algorithms.implementation;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class LibraryFine {
+
+    static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+    	int cost=0;
+    	if(y1!=y2 && y2<y1)
+    	{
+    		cost=10000;
+    	}
+    	else if(y1==y2){
+    		if(m1!=m2 && m2<m1)
+    		{
+    			cost=500*(m1-m2);
+    		}
+    		else if(m1==m2){
+    			if(d1!=d2 && d2<d1)
+    			{
+    				cost=15*(d1-d2);
+    			}
+    		}
+    	}
+		return cost;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int d1 = in.nextInt();
+        int m1 = in.nextInt();
+        int y1 = in.nextInt();
+        int d2 = in.nextInt();
+        int m2 = in.nextInt();
+        int y2 = in.nextInt();
+        int result = libraryFine(d1, m1, y1, d2, m2, y2);
+        System.out.println(result);
+        in.close();
+    }
+}
